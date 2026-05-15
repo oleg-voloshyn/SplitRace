db980b914537322ede2d376bdcdbd077e6e85b54
oleg-voloshyn <voloshyn@active-bridge.com>
2026-05-15 19:47:01 +0300

    fix delete tournament

--- app/controllers/admin/tournaments_controller.rb
+++ app/controllers/admin/tournaments_controller.rb
@@ -62,8 +62,8 @@ module Admin
     def destroy
       @tournament.destroy!
       redirect_to admin_tournaments_path, notice: 'Deleted.'
-    rescue ActiveRecord::InvalidForeignKey, ActiveRecord::RecordNotDestroyed => e
-      redirect_to admin_tournaments_path, alert: "Tournament could not be deleted: #{e.message}"
+    rescue => e
+      redirect_to admin_tournament_path(@tournament), alert: "Tournament could not be deleted: #{e.message}"
     end
 
     def activate
